9655a351250c6d73fb623c59eaf9de90c125d3b7
ChristianSantangelo <95893342+ChristianSantangelo@users.noreply.github.com>
2021-12-20 12:32:58 -0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(); // Fill in
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
